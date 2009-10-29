a582cc5c47d8ba993b6726dad39316d8fc91b793
Rich Wareham <rjw57@cam.ac.uk>
2009-10-29 16:46:16 +0000

    Latitude/longitude window is better specified.
    The latitude and longitude window for get_wind_data.py is now better specified
    as a centre and radius.
    
    Also the code itself saves in a better format for subsequent reading.

--- pydap/get_wind_data.py
+++ pydap/get_wind_data.py
@@ -4,10 +4,7 @@
 import datetime, math, sys, logging, calendar, optparse
 
 # We use Pydap from http://pydap.org/.
-import pydap.exceptions, pydap.client, pydap.lib
-
-# Use a client-side cache
-pydap.lib.CACHE = 'gfs-cache/'
+import pydap.exceptions, pydap.client
 
 # Output logger format
 log = logging.getLogger('main')
@@ -27,29 +24,64 @@ def main():
         metavar='TIME',
         default=calendar.timegm(datetime.datetime.utcnow().timetuple()))
     parser.add_option('-o', '--output', dest='output',
-        help='file to write output to or \'-\' for stdout \t[default: stdout]',
+        help='file to write output to with \'%(VAR)\' replaced with the the value of VAR [default: %default]',
         metavar='FILE',
-        default='-')
-    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
-        help='be verbose \t[default: %default]', default=False)
+        default='gfs/gfs_%(time)_%(lat)_%(lon)_%(latdelta)_%(londelta).dat')
+    parser.add_option('-v', '--verbose', action='count', dest='verbose',
+        help='be verbose. The more times this is specified the more verbose.', default=False)
     parser.add_option('-p', '--past', dest='past',
-        help='number of hours in past to start saving data \t[default: %default]',
+        help='window of time to save data is at most HOURS hours in past [default: %default]',
         metavar='HOURS',
-        default='3')
+        type='int', default=3)
     parser.add_option('-f', '--future', dest='future',
-        help='number of hours in future to stop saving data \t[default: %default]',
+        help='window of time to save data is at most HOURS hours in future [default: %default]',
         metavar='HOURS',
-        default='6')
+        type='int', default=9)
+
+    group = optparse.OptionGroup(parser, "Location specifiers",
+        "Use these options to specify a particular window of data to download.")
+    group.add_option('--lat', dest='lat',
+        help='window centre latitude in range (-90,90) degrees north [default: %default]',
+        metavar='DEGREES',
+        type='float', default=52)
+    group.add_option('--lon', dest='lon',
+        help='window centre longitude in degrees east [default: %default]',
+        metavar='DEGREES',
+        type='float', default=0)
+    group.add_option('--latdelta', dest='latdelta',
+        help='window radius in latitude in degrees [default: %default]',
+        metavar='DEGREES',
+        type='float', default=6)
+    group.add_option('--londelta', dest='londelta',
+        help='window radius in longitude in degrees [default: %default]',
+        metavar='DEGREES',
+        type='float', default=6)
+    parser.add_option_group(group)
 
     (options, args) = parser.parse_args()
 
-    if options.verbose:
-        log.setLevel(logging.INFO)
+    # Check the latitude is in the right range.
+    if (options.lat < -90) | (options.lat > 90):
+        log.error('Latitude %s is outside of the range (-90,90).')
+        sys.exit(1)
 
-    if options.output == '-':
-        output_file = sys.stdout
-    else:
-        output_file = open(options.output, 'w')
+    # Check the delta sizes are valid.
+    if (options.latdelta <= 0.5) | (options.londelta <= 0.5):
+        log.error('Latitiude and longitude deltas must be at least 0.5 degrees.')
+        sys.exit(1)
+
+    if options.londelta > 180:
+        log.error('Longitude window sizes greater than 180 degrees are meaningless.')
+        sys.exit(1)
+
+    # We need to wrap the longitude into the right range.
+    options.lon = canonicalise_longitude(options.lon)
+
+    # How verbose are we being?
+    if options.verbose > 0:
+        log.setLevel(logging.INFO)
+    if options.verbose > 1:
+        log.setLevel(logging.DEBUG)
 
     timestamp_to_find = options.timestamp
     time_to_find = datetime.datetime.utcfromtimestamp(timestamp_to_find)
@@ -70,48 +102,125 @@ def main():
     log.info('      End time: %s (POSIX %s)' % \
         (dataset_times[-1].ctime(), dataset_timestamps[-1]))
 
-    log.debug('      Latitude: %s -> %s' % (min(dataset.lat), max(dataset.lat)))
-    log.debug('     Longitude: %s -> %s' % (min(dataset.lon), max(dataset.lon)))
+    log.info('      Latitude: %s -> %s' % (min(dataset.lat), max(dataset.lat)))
+    log.info('     Longitude: %s -> %s' % (min(dataset.lon), max(dataset.lon)))
 
-    write_file(output_file, dataset, ('hgtprs', 'ugrdprs', 'vgrdprs'), \
-        -2, 2, 50, 54, # FIXME: Don't hardcode these!
-        time_to_find - datetime.timedelta(hours=int(options.past)),
-        time_to_find + datetime.timedelta(hours=int(options.future)))
+    window = (options.lat, options.latdelta, options.lon, options.londelta)
 
-def write_file(output, data, vars, minlat, maxlat, minlon, maxlon, mintime, maxtime):
-    for var in vars:
+    write_file(options.output, dataset, \
+        window, \
+        time_to_find - datetime.timedelta(hours=options.past), \
+        time_to_find + datetime.timedelta(hours=options.future))
+
+def write_file(output_format, data, window, mintime, maxtime):
+    log.info('Downloading data in window (lat, lon) = (%s +/- %s, %s +/- %s).' % window)
+    downloaded_data = { }
+    for var in ('hgtprs', 'ugrdprs', 'vgrdprs'):
         grid = data[var]
         log.info('Processing variable \'%s\' with shape %s...' % (var, grid.shape))
 
         # Check the dimensions are what we expect.
         assert(grid.dimensions == ('time', 'lev', 'lat', 'lon'))
 
-        times= map(timestamp_to_datetime, grid.maps['time'])
+        # Work out what times we want to download
+        times = filter(lambda x: (x >= mintime) & (x <= maxtime),
+            map(timestamp_to_datetime, grid.maps['time']))
 
-        for timeidx, time in enumerate(times):
-            # If this is outside of our time window, skip the record.
-            if (time < mintime) | (time > maxtime):
-                continue
+        start_time = min(times)
+        end_time = max(times)
+        log.info('   Downloading from %s to %s.' % (start_time.ctime(), end_time.ctime()))
 
-            timestamp = datetime_to_posix(time)
-            log.info('   Downloading data for %s...' % time.ctime())
-            selection = grid[timeidx,:,\
-                (grid.lat >= minlat) & (grid.lat <= maxlat),\
-                (grid.lon >= minlon) & (grid.lon <= maxlon)]
-            log.debug('   Downloaded data has shape %s...', selection.shape)
-            log.debug('   Writing data...')
-            output.write('CHUNK START\n')
-            output.write('NAME %s\n' % var)
-            output.write('DATALINECOUNT %s\n' % reduce(lambda x, y: x * y, selection.shape))
-            output.write('DATAFIELDS %s\n' % ','.join(('TIME','PRESSURE','LATITUDE','LONGITUDE','VALUE')))
-            for pressureidx, pressure in enumerate(selection.maps['lev']):
-                for latidx, latitude in enumerate(selection.maps['lat']):
-                    for lonidx, longitude in enumerate(selection.maps['lon']):
-                        record = ( timestamp, pressure, \
-                                   latitude, longitude, \
-                                   selection.array[pressureidx,latidx,lonidx] )
-                        output.write('DATA %s\n' % ','.join(map(str,record)))
-            output.write('CHUNK END\n')
+        # Download the data. Unfortunately, OpeNDAP only supports remote access of
+        # contiguous regions. Since the longitude data wraps, we may require two 
+        # windows. The 'right' way to fix this is to download a 'left' and 'right' window
+        # and munge them together. In terms of download speed, however, the overhead of 
+        # downloading is so great that it is quicker to download all the longitude 
+        # data in our slice and do the munging afterwards.
+        selection = grid[\
+            times.index(start_time):(times.index(end_time)+1), \
+            :, \
+            (grid.lat >= (window[0]-window[1])) & (grid.lat <= (window[0]+window[1])), \
+            : ]
+
+        # Cache the downloaded data for later
+        downloaded_data[var] = selection
+
+        log.info('   Downloaded data has shape %s...', selection.shape)
+        if len(selection.shape) != 4:
+            log.error('    Expected 4-d data. Perhaps your time window is too short?')
+            return
+
+    # Check all the downloaded data has the same shape
+    target_shape = downloaded_data['hgtprs']
+    assert( all( map( lambda x: x == target_shape, 
+        filter( lambda x: x.shape, downloaded_data.itervalues() ) ) ) )
+
+    log.info('Writing output...')
+
+    hgtprs = downloaded_data['hgtprs']
+    ugrdprs = downloaded_data['ugrdprs']
+    vgrdprs = downloaded_data['vgrdprs']
+
+    # Filter the longitudes we're actually going to use.
+    longitudes = filter(lambda x: longitude_distance(x[1], window[2]) <= window[3] ,
+                        enumerate(hgtprs.maps['lon']))
+
+    log.debug('Using longitudes: %s' % (map(lambda x: x[1], longitudes),))
+
+    # Write one file for each time index.
+    for timeidx, time in enumerate(hgtprs.maps['time']):
+        timestamp = datetime_to_posix(timestamp_to_datetime(time))
+
+        output_filename = output_format
+        output_filename = output_filename.replace('%(time)', str(timestamp))
+        output_filename = output_filename.replace('%(lat)', str(window[0]))
+        output_filename = output_filename.replace('%(latdelta)', str(window[1]))
+        output_filename = output_filename.replace('%(lon)', str(window[2]))
+        output_filename = output_filename.replace('%(londelta)', str(window[3]))
+
+        log.info('   Writing \'%s\'...' % output_filename)
+        output = open(output_filename, 'w')
+
+        # Write the header
+        output.write('# window centre latitude, window latitude radius, window centre longitude, window longitude radius, POSIX timestamp\n')
+        header = window + (timestamp,)
+        output.write(','.join(map(str,header)) + '\n')
+
+        # Write the number of lines of data
+        output.write('# num_lines\n')
+        output.write('%s\n' % (hgtprs.shape[1] * hgtprs.shape[2] * len(longitudes)))
+
+        # Write the data itself.
+        output.write('# latitude, longitude, geopotential height [gpm], u-component wind [m/s], v-component wind [m/s]\n')
+        for pressureidx, pressure in enumerate(hgtprs.maps['lev']):
+            for latidx, latitude in enumerate(hgtprs.maps['lat']):
+                for lonidx, longitude in longitudes:
+                    record = ( latitude, longitude, 
+                               hgtprs.array[timeidx,pressureidx,latidx,lonidx], \
+                               ugrdprs.array[timeidx,pressureidx,latidx,lonidx], \
+                               vgrdprs.array[timeidx,pressureidx,latidx,lonidx] )
+                    output.write(','.join(map(str,record)) + '\n')
+
+def canonicalise_longitude(lon):
+    """
+    The GFS model has all longitudes in the range 0.0 -> 359.5. Canonicalise
+    a longitude so that it fits in this range and return it.
+    """
+    lon = math.fmod(lon, 360)
+    if lon < 0.0:
+        lon += 360.0
+    assert((lon >= 0.0) & (lon < 360.0))
+    return lon
+
+def longitude_distance(lona, lonb):
+    """
+    Return the shortest distance in degrees between longitudes lona and lonb.
+    """
+    distances = ( \
+        math.fabs(lona - lonb),  # Straightforward distance
+        360 - math.fabs(lona - lonb), # Other way 'round.
+    )
+    return min(distances)
 
 def datetime_to_posix(time):
     """
