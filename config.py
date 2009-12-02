83b64d7f2cfdc2e66eb1bfcca5af2cf1ec647276
Rich Wareham <rjw57@cam.ac.uk>
2009-12-02 13:57:26 +0000

    Add script to list GFS file timestamps

---
+++ config.py
@@ -0,0 +1,10 @@
+# This file contains constants describing the locations of various utilities on the SRCF
+
+import os
+
+CUSF_HOME = '/societies/cuspaceflight/'
+LAND_PRED_APP = os.path.join(CUSF_HOME, 'git/cusf-landing-prediction/pred_src/pred')
+LAND_PRED_DATA = os.path.join(CUSF_HOME, 'landing-prediction-data/gfs/')
+
+# vim:sw=4:ts=4:et:autoindent
+
