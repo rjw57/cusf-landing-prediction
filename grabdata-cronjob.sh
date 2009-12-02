3148d68450842824a7ed72fb9aa5df1f2226c3b1
Rich Wareham <rjw57@cam.ac.uk>
2009-12-02 13:42:21 +0000

    Purge old data files

--- grabdata-cronjob.sh
+++ grabdata-cronjob.sh
@@ -41,4 +41,8 @@ fi
 LOGFILE=${LOGDIR}/fetchdatalog-`date +%Y.%m.%d-%H:%M:%S`
 
 # Run the data grabber from now to 24 hours in future
-${GETDATA} --lat=52 --lon=0 -v -f 24 2>${LOGFILE}
+${GETDATA} --lat=52 --lon=0 --latdelta=10 --londelta=10 -v -f 24 2>${LOGFILE}
+
+# Delete any data that hasn't been changed for 3 days. This stops us filling
+# the CUSF quota with old atmosphere data.
+find ${GFDSIR} -mtime 3 -name 'gfs*' | xargs rm -f
